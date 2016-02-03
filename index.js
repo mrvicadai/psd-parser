97b3c8cac5481e6d0e0c06cdd3dfb7933be89d54
Aaron Dai <mrvicadai@gmail.com>
2016-02-03 15:05:37 -0800

    use multilayer image

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var PSD = require('psd');
+// var detect = require('charset-detector');
+var Buffer = require('buffer').Buffer;
+var Iconv  = require('iconv').Iconv;
+var assert = require('assert');
+
 
 var PSD_FILE = "psd-export-sample.psd";
 
 // You can also use promises syntax for opening and parsing
 PSD.open(PSD_FILE).then(function(psd) {
-  console.log(psd.tree());
-  // console.log(psd.image);
-}).then(function() {
-  console.log("Finished!");
+  // console.log(psd.tree());
+  node = psd.tree().descendants()[1];
+  console.log(JSON.stringify(psd.tree().export(), null , 4));
 });
 
