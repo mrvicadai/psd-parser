cfda95a25b7ec149cdd354841c1bfe350957d46a
Aaron Dai <mrvicadai@gmail.com>
2016-02-03 15:12:37 -0800

    add capability to export to png

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var PSD = require('psd');
 // var detect = require('charset-detector');
 var Buffer = require('buffer').Buffer;
-var Iconv  = require('iconv').Iconv;
+var Iconv = require('iconv').Iconv;
 var assert = require('assert');
 
 
@@ -11,6 +11,14 @@ var PSD_FILE = "psd-export-sample.psd";
 PSD.open(PSD_FILE).then(function(psd) {
   // console.log(psd.tree());
   node = psd.tree().descendants()[1];
-  console.log(JSON.stringify(psd.tree().export(), null , 4));
+  console.log(JSON.stringify(psd.tree().export(), null, 4));
+  saveToImage(psd);
 });
+
+function saveToImage(psd) {
+  var png = psd.image.toPng(); // get PNG object
+  psd.image.saveAsPng('output.png').then(function() {
+    console.log('Exported!');
+  });
+}
 
